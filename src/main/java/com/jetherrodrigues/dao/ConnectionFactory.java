f3ac7d6292f587e1f002af35d24f3f528be0bc50
Jether Rodrigues <jetherrodrigues@gmail.com>
2020-05-19 22:13:10 -0300

    changed whizlab to whizlabs

--- src/main/java/com/jetherrodrigues/dao/ConnectionFactory.java
+++ src/main/java/com/jetherrodrigues/dao/ConnectionFactory.java
@@ -10,7 +10,7 @@ public final class ConnectionFactory implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpql-whizlab");
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpql-whizlabs");
 	
 	public static EntityManager getEntityManager() {
 		return emf.createEntityManager();
